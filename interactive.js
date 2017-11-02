5e4c62a64e3155801a11b0fa7e37fae5777809db
TheHopefulMarshmallow <31549522+TheHopefulMarshmallow@users.noreply.github.com>
2017-11-02 13:35:40 -0400

    work in progress.

---
+++ interactive.js
@@ -0,0 +1,15 @@
+var everythingTicket = 42
+var openTicket = 0
+var collegeTicket = 10
+var highschoolTicket = 0
+var amountDonated = 
+var ticketsPurchased =
+var costTicketsPurchased = ('$' + everythingTicket*ticketsPurchased + collegeTicket*ticketsPurchased)
+var totalCost =
+console.log(everythingTicket)
+console.log(openTicket)
+console.log(collegeTicket)
+console.log(highschoolTicket)
+console.log(amountDonated)
+console.log(ticketsPurchased)
+console.log(costTicketsPurchased)
